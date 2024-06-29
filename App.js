16912ca9f05494c2b70a7e2cebff4f7cf280a3d7
athenashrikant <shrikant.jha@athenaeducation.co>
2024-06-29 12:30:38 +0530

    text style + responsive

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={{
-      width: '100%',
-      height: '100%',
-    }}>
-      <Text>Test</Text>
-      <HomeScreen />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { colorPallet } from './../theme/colorPallet'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const HomeScreen = () => {
   return (
     <View style={{
-        backgroundColor: 'black ',
-        flex: 1,
+      width: wp(100),
+      height: wp(50),
+      backgroundColor: 'skyblue',
+      justifyContent: 'center',
+      alignItems: 'center'
     }}>
-      <Text>HomeScreen</Text>
+      <Text style= {{
+        fontSize: wp(10),
+        fontWeight: 'bold',
+        color: 'white'
+      }}>HomeScreen</Text>
     </View>
   )
 }
