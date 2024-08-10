7ccfe86cdb5caf99078fed6e6ff88de1747c2ea4
athenashrikant <shrikantjha11@gmail.com>
2024-08-10 17:18:24 +0530

    Update StoryScreen.js

--- src/screens/StoryScreen.js
+++ src/screens/StoryScreen.js
@@ -1,28 +1,66 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const StoryScreen = () => {
   return (
-    <View style={styles.fullScreen}>
-      
+    <ScrollView contentContainerStyle={styles.fullScreen}>
       {/* IMAGE */}
       <View style={styles.coverImage}>
         <Image
-          source={{ uri: `https://i.pinimg.com/originals/80/ae/d6/80aed6c86934b5cbbd5bcb2502ea5acc.jpg` }}
+          source={{
+            uri: `https://i.pinimg.com/originals/80/ae/d6/80aed6c86934b5cbbd5bcb2502ea5acc.jpg`,
+          }}
           style={styles.coverImage}
         />
       </View>
 
-      
+      {/* MAIN STORY TEXT */}
+      <View>
+        <Text
+          style={{
+            fontSize: wp(5),
+            marginTop: wp(5),
+          }}
+        >
+          The story is about friendship, fear, relationship, unreliability, and
+          honesty. It’s about a doctor named Dr. Raman who believes that lies
+          can’t save the life of a patient and as it is traditional in a
+          profession of a doctor, {`\n`}{`\n`} He should never lie to the patient means if
+          someone is about to die then a doctor should tell him the truth. One
+          day, he comes to know about his childhood friend, Gopal. They both
+          knew each other for forty years.{`\n`}{`\n`} They met occasionally as both were
+          busy with their work and families.
+        </Text>
 
 
-    </View>
-  )
-}
+
+        <Text
+          style={{
+            fontSize: wp(5),
+            marginTop: wp(5),
+          }}
+        >
+          The story is about friendship, fear, relationship, unreliability, and
+          honesty. It’s about a doctor named Dr. Raman who believes that lies
+          can’t save the life of a patient and as it is traditional in a
+          profession of a doctor, {`\n`}{`\n`} He should never lie to the patient means if
+          someone is about to die then a doctor should tell him the truth. One
+          day, he comes to know about his childhood friend, Gopal. They both
+          knew each other for forty years.{`\n`}{`\n`} They met occasionally as both were
+          busy with their work and families.
+        </Text>
+       
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-  fullScreen : {
+  fullScreen: {
     width: wp(100),
     height: hp(100),
     // backgroundColor: 'pink',
@@ -32,11 +70,11 @@ const styles = StyleSheet.create({
   coverImage: {
     width: wp(90),
     height: wp(90),
-    backgroundColor: 'red',
+    backgroundColor: "red",
     // marginHorizontal: wp(5),
     borderRadius: wp(5),
     elevation: wp(5),
-  }
-})
+  },
+});
 
-export default StoryScreen
+export default StoryScreen;
