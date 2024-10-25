0ac2e9ec2df132b9d3840f9b60a2679957ac439c
devtanmayjha <tanmayj1802@gmail.com>
2024-10-25 18:47:33 +0530

    Update EditProfileScreen.js

--- src/screens/profile/EditProfileScreen.js
+++ src/screens/profile/EditProfileScreen.js
@@ -10,8 +10,8 @@ import React from "react";
 import { Appbar, Button } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
 // https://www.behance.net/gallery/108433365/Profile-screen?tracking_source=search_projects|profile+screen&l=3
@@ -21,58 +21,97 @@ const EditProfileScreen = () => {
     <SafeAreaProvider>
       <SafeAreaView>
         <Appbar.Header>
-          <Appbar.BackAction onPress={() => {}} />
+          <Appbar.BackAction onPress={() => { }} />
           <Appbar.Content title="Your Profile" />
-          <Appbar.Action icon="pencil" onPress={() => {}} />
-          <Appbar.Action icon="magnify" onPress={() => {}} />
+          <Appbar.Action icon="pencil" onPress={() => { }} />
+          <Appbar.Action icon="magnify" onPress={() => { }} />
         </Appbar.Header>
 
         {/* PROFILE SETTINGS */}
         <View style={{
-            width: wp(100),
+          width: wp(100),
         }}>
-            <Text style={{
-                fontWeight: '500',
-                fontSize: wp(6),
-                margin: wp(5),
-            }}>Profile Settings</Text>
+          <Text style={{
+            fontWeight: '500',
+            fontSize: wp(6),
+            margin: wp(5),
+          }}>Profile Settings</Text>
         </View>
 
         {/* PROFILE PHOTO */}
         <View style={{
-            width: wp(100),
-            justifyContent: 'center',
-            alignItems: 'center',
+          width: wp(100),
+          justifyContent: 'center',
+          alignItems: 'center',
         }}>
-            <View style={{ 
-                width: wp(30),
-                height: wp(30),
-                backgroundColor: 'pink',
-                borderRadius: wp(4),
-                overflow: 'hidden',
-            }}>
-                <Image style={{ flex: 1 }} source={{ uri : 'https://cbin.b-cdn.net/img/D/D%20Gukesh_JHWTS_1000x667.jpeg' }} />
-            </View>
-            <Text style={{
-                marginVertical: wp(2.5),
-                fontSize: wp(3.7),
-            }}>change profile photo</Text>
+          <View style={{
+            width: wp(30),
+            height: wp(30),
+            backgroundColor: 'pink',
+            borderRadius: wp(4),
+            overflow: 'hidden',
+          }}>
+            <Image style={{ flex: 1 }} source={{ uri: 'https://cbin.b-cdn.net/img/D/D%20Gukesh_JHWTS_1000x667.jpeg' }} />
+          </View>
+          <Text style={{
+            marginVertical: wp(2.5),
+            fontSize: wp(3.7),
+          }}>change profile photo</Text>
         </View>
 
 
 
-            <View style={{
-                width: wp(100),
-            }}>
+        <View style={{
+          width: wp(100),
+        }}>
 
-            </View>
-        
+        </View>
+
+        <View>
+          <TextInput style={{
+            borderWidth: 5,
+            borderRadius: 20,
+            margin: 10,
+            padding: 10,
+          }}>
+            First name
+          </TextInput>
+
+          <TextInput style={{
+            borderWidth: 5,
+            borderRadius: 20,
+            margin: 10,
+            padding: 10,
+          }}>
+            Last name
+          </TextInput>
+
+          <TextInput style={{
+            borderWidth: 5,
+            borderRadius: 20,
+            margin: 10,
+            padding: 10,
+          }}>
+            Email
+          </TextInput>
+
+          <Button style={{
+            borderWidth: 5,
+            borderColor: 'black',
+            margin: 10,
+            marginTop: 80,
+          }}>
+            Save Changes
+          </Button>
+
+        </View>
 
 
 
-        <View style={styles.profile}>
-          {/* <Text style={styles.title}>Your Profile</Text> */}
-          <View style={styles.circle}>
+
+        {/* <View style={styles.profile}> */}
+        {/* <Text style={styles.title}>Your Profile</Text> */}
+        {/* <View style={styles.circle}>
             <TextInput
               style={{
                 borderColor: "green",
@@ -80,17 +119,17 @@ const EditProfileScreen = () => {
                 alignSelf: "",
               }}
             ></TextInput>
-          </View>
+          </View> */}
 
-          <Text style={styles.text}>Your Username:</Text>
+        {/* <Text style={styles.text}>Your Username:</Text>
           <TextInput style={styles.textinput} placeholder="Username..." />
 
           <Text style={styles.text}>Your Email-id:</Text>
           <TextInput style={styles.textinput} placeholder="abcd@gmail.com" />
 
           <Text style={styles.text}>Your password:</Text>
-          <TextInput style={styles.textinput} placeholder="password..." />
-        </View>
+          <TextInput style={styles.textinput} placeholder="password..." /> */}
+        {/* </View> */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
